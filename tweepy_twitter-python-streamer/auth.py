ccef253cd64cb7a81a21a21c49c095d34674726b
data2d3 <80802632+data2d3@users.noreply.github.com>
2021-03-16 23:43:22 -0400

    Add files via upload
    streaming tweets with tweepy

---
+++ tweepy_twitter-python-streamer/auth.py
@@ -0,0 +1,12 @@
+
+class TwitterAuth:
+	# Go to http://dev.twitter.com and create an app. 
+	# The consumer key and secret will be generated for you after
+	consumer_key="YOUR KEY"
+	consumer_secret="YOUR KEY"
+
+	# After the step above, you will be redirected to your app's page.
+	# Create an access token under the the "Your access token" section
+	access_token="YOUR KEY"
+	access_token_secret="YOUR KEY"
+
